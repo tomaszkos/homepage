7887b6a2d43254fa7a986dcd3ca42cef92bf31a2
Tomek Kos <TKos@interia.eu>
2019-06-23 11:36:26 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//alert('siemanko byku!');
+console.log('Siemanko');
+const project ='Homepage';
+const name = 'Tomek';
+const age = 20;
+//console.log(name, age);
+console.log(`Jestem ${name} i mam ${age} lat.
+a to co widzisz to mój projekt ${project}.
+Miłego kontemplowania.`)
+
+let kozak = null;
+//console.log(kozak);
